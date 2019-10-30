7651a47e1ab1baeba4c45f81aa6f63c32d0a9add
Jordan <jordandoan@hotmail.com>
2019-10-30 08:57:08 -0700

    More think6.5 stuff

--- keyboards/gray_studio/think65/hotswap/keymaps/jordan/keymap.c
+++ keyboards/gray_studio/think65/hotswap/keymaps/jordan/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                         KC_TRNS, KC_TRNS, KC_TRNS,              KC_RALT, KC_TRNS,         KC_MS_L, KC_MS_D, KC_MS_R  
     ),
 [2] = LAYOUT_65_ansi_blocker( 
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_TRN, KC_MUTE, 
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_TRNS, KC_MUTE, 
         KC_TRNS,          KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_TRNS, 
         KC_TRNS,          VLK_TOG, KC_TRNS, KC_TRNS, KC_TRNS, LALT(KC_F4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       
         KC_TRNS, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, KC_TRNS, KC_BTN1,        KC_PGUP,
